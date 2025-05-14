afe05cc38177690d2e0365f72d5a9d9b2c25a7aa
PongsapakAu <pongsapak.0895@gmail.com>
2025-05-14 17:13:58 +0700

    project

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,7 +2,7 @@ import { Description } from "../libs/resumeinfo";
 
 export default function About() {
   return (
-    <div className="bg-cradbg flex items-center justify-center rounded-xl px-[80px] py-[60px] text-white shadow-xl transition-all duration-500 md:flex-col xl:flex-row">
+    <div className="bg-cradbg flex items-center justify-center rounded-xl px-[80px] py-6 text-white shadow-xl transition-all duration-500 md:flex-col xl:flex-row">
       <div className="flex max-w-2xl flex-col">
         <h1 className="relative flex items-center gap-4 pb-5 text-5xl font-bold uppercase">
           About Me
@@ -15,8 +15,6 @@ export default function About() {
           ))}
         </div>
       </div>
-      <div>test</div>
-
       <img
         src="/profile/raw.png"
         className="rounded-full border-4 border-cyan-400 object-cover shadow-md transition-transform duration-300 hover:scale-105 md:h-32 md:w-32 lg:h-64 lg:w-64"
