6f5a048910e199cffecb0809ddd901e681cc80ba
Bew60000 <anw.nattapon.w@gmail.com>
2025-05-15 15:41:54 +0700

    Edit text in "about" Page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,11 +1,16 @@
-import { Descriptions } from "../libs/resumeinfo";
+import { Descriptions, name } from "../libs/resumeinfo";
 
 export default function About() {
   return (
     <div className="bg-cradbg/30 flex items-center justify-center gap-6 rounded-xl border-[0.5px] border-amber-50/20 px-24 py-12 text-white shadow-xl transition-all duration-500 md:flex-col xl:flex-row">
       <div className="flex flex-col">
-        <p className="relative flex items-center gap-4 pb-5 text-5xl font-bold uppercase">
-          About Me
+        <p className="py-4 text-5xl font-bold uppercase">
+          {/* About Me */}
+          {name.map((Name, index) => (
+            <p key={index}>
+              {Name.fullName}:<span className="">{Name.nickName}</span>
+            </p>
+          ))}
         </p>
 
         <div className="space-y-4 text-lg text-[14px] leading-relaxed tracking-wide whitespace-pre-wrap">
